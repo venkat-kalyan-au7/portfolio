5bb44d265189ed291921288f1a76a637bac50928
Venkat Kalyan <60262281+venkat-kalyan-au7@users.noreply.github.com>
2020-09-19 13:03:55 +0530

    Updates

---
+++ precache-manifest.5ddf05a53768ed093e6c76252106f970.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0e7bfef95b64cf52fdd9cd4fbc2fc4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a9daca25344fd569e4c",
+    "url": "/portfolio/static/css/main.8aa39063.chunk.css"
+  },
+  {
+    "revision": "bc55af10cf7709efdcd2",
+    "url": "/portfolio/static/js/2.6ce2dad4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.6ce2dad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9daca25344fd569e4c",
+    "url": "/portfolio/static/js/main.a2c2a18e.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "2c2b9e79b84add2029938fb08f0d1307",
+    "url": "/portfolio/static/media/Screenshot (56).2c2b9e79.png"
+  },
+  {
+    "revision": "5a87686a19198a9948fdd2a5b4886624",
+    "url": "/portfolio/static/media/Screenshot (57).5a87686a.png"
+  },
+  {
+    "revision": "4c61caf52cc8b3ab5f3ab0b1552fd83f",
+    "url": "/portfolio/static/media/Screenshot (58).4c61caf5.png"
+  },
+  {
+    "revision": "d8f05eb4bc31a72ffe5b0f5b15e1243e",
+    "url": "/portfolio/static/media/Screenshot (59).d8f05eb4.png"
+  },
+  {
+    "revision": "5f3225395f1c5e26e2acce8511fed7aa",
+    "url": "/portfolio/static/media/profile.5f322539.jpg"
+  }
+]);
